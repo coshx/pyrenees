fc3b095d41c33b2b7335de1f2da8c17e82276aef
Adrien Cadet <acadet@live.fr>
2016-02-10 18:13:49 -0800

    Scaffolds library

---
+++ pyrenees/pyrenees.h
@@ -0,0 +1,19 @@
+//
+//  pyrenees.h
+//  pyrenees
+//
+//  Created by Adrien on 10/02/16.
+//  Copyright © 2016 Coshx Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pyrenees.
+FOUNDATION_EXPORT double pyreneesVersionNumber;
+
+//! Project version string for pyrenees.
+FOUNDATION_EXPORT const unsigned char pyreneesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pyrenees/PublicHeader.h>
+
+
